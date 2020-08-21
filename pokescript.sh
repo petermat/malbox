7f985fa7e5c869b4f815ef60866f617341e04ac3
Peter Matkovski <petermat@users.noreply.github.com>
2020-08-21 18:14:41 +0200

    Add files via upload

---
+++ pokescript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js
+sleep 5
+curl https://www.cyberciti.biz/faq/linux-unix-sleep-bash-scripting/
+speep 5
+curl https://stackoverflow.com/questions/42716677/how-to-update-javascript-excel-api-set-to-1-4/42725444#42725444
+touch /vagrant/malfile
+chmod +x /vagrant/malfile
+sleep 2
+rm /vagrant/malfile
